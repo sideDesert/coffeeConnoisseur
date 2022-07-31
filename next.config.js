760a5a585d801fef13947683046df00857464687
Siddarth Saha <siddarthsaha1@gmail.com>
2022-07-31 09:41:59 +0530

    enabled strict mode

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["images.unsplash.com"],
